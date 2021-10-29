00b3b30cb89f560273847941e2373de4f0bd021f
keithcharlesc <keithcharleschan@gmail.com>
2021-10-29 16:34:06 +0800

    Merge origin/master
    Conflicts:
    	HotelReservationSystemLibrary/src/entity/NightEntity.java

---
+++ HotelReservationSystemLibrary/src/util/exception/DeleteEmployeeException.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util.exception;
+
+/**
+ *
+ * @author xianhui
+ */
+public class DeleteEmployeeException extends Exception {
+
+    public DeleteEmployeeException() {
+    }
+
+    public DeleteEmployeeException(String string) {
+        super(string);
+    }
+    
+    
+    
+}
